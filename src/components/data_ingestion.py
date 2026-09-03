e204d5b931ff1b47b2b5a6105f4031837fca5ba7
Labina25 <labinas2568@gmail.com>
2026-09-03 08:14:19 +0530

    some error updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -15,8 +15,8 @@ from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path : str=os.path.join('artifacts',"train.csv")
-    test_data_path :  str=os.path.join('artifacts',"test.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 
@@ -28,7 +28,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or components")
 
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv(r'D:\mlproject\notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -56,7 +56,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer= ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,7 +28,7 @@ class DataTransformation:
         '''
 
         try:
-            numerical_columns=["writing_score", "reading_score"]
+            numerical_columns=["writing_score","reading_score"]
             categorical_columns = [
                 "gender",
                 "race_ethnicity",
@@ -80,18 +80,18 @@ class DataTransformation:
             target_column_name ="math_score"
             numerical_columns= ["writing_score","reading_score"]
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name])
             target_feature_test_df = test_df[target_column_name]
 
             logging.info(
-                f"Applying preprocessing object on training datframe and testing dataframe."
+                f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_arr)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
